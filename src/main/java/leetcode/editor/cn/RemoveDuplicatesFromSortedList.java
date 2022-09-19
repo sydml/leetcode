eb6cfdaee1640855d69994e3c33abec895018928
liuyuming <Lym!XJ0301>
2022-09-20 00:46:42 +0800

    20220919答题

---
+++ src/main/java/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,73 @@
+package leetcode.editor.cn;
+/**
+ * author: liuyuming
+ * date: 2022-09-19 23:36:51
+ */
+//给定一个已排序的链表的头
+// head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序 排列 
+// 
+//
+// Related Topics 链表 👍 856 👎 0
+
+public class RemoveDuplicatesFromSortedList{
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            if (fast.val != slow.val) {
+                // nums[slow] = nums[fast];
+                slow.next = fast;
+                // slow++;
+                slow = slow.next;
+            }
+            // fast++
+            fast = fast.next;
+        }
+        // 断开与后面重复元素的连接
+        slow.next = null;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public static void main(String[] args) {
+	    Solution solution = new RemoveDuplicatesFromSortedList().new Solution();
+	}
+}
